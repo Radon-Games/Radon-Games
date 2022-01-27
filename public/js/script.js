e5196eb4beae8bf8ec3b2870e5e4fda1639dc039
ericksoncohen <72945444+ericksoncohen@users.noreply.github.com>
2022-01-26 20:16:34 -0600

    brrr

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,22 @@
-// settings
-let settings = JSON.parse(window.localStorage.getItem("settings")) || {
-  tabAutoHide: false,
-  tabAutoHideIconURL: "https://google.com/favicon.ico",
-  tabAutoHideTitle: "Google"
-};
+// // settings
+// let settings = JSON.parse(window.localStorage.getItem("settings")) || {
+//   tabAutoHide: false,
+//   tabAutoHideIconURL: "https://google.com/favicon.ico",
+//   tabAutoHideTitle: "Google"
+// };
 
-// Tab Visiblilty
-document.addEventListener("visibilitychange", function () {
-  if(settings.tabAutoHide) {
-    let icon = document.getElementById("icon");
-    if (document.hidden) {
-      icon.href = settings.tabAutoHideIconURL;
-      document.title = settings.tabAutoHideTitle;
-    } else {
-      icon.href = "https://cdn.glitch.global/7a7ecda1-7fea-4235-b2da-81abaec6b102/logo-512.png";
-      document.title = "Radon Games";
-    }
-  }
-});
+// // Tab Visiblilty
+// document.addEventListener("visibilitychange", function () {
+//   if(settings.tabAutoHide) {
+//     let icon = document.getElementById("icon");
+//     if (document.hidden) {
+//       icon.href = settings.tabAutoHideIconURL;
+//       document.title = settings.tabAutoHideTitle;
+//     } else {
+//       icon.href = "img/logo-512.png";
+//       document.title = "Radon Games";
+//     }
+//   }
+// });
 
-window.localStorage.setItem("settings", JSON.stringify(settings));
+// window.localStorage.setItem("settings", JSON.stringify(settings));
