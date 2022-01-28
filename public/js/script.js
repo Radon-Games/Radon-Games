e2e7c73ef2bac1e3f841b8fff83c2cdb2c35b1b3
Cohen Erickson <72945444+ericksoncohen@users.noreply.github.com>
2022-01-28 21:10:12 +0000

    updates

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,54 @@
-// // settings
-// let settings = JSON.parse(window.localStorage.getItem("settings")) || {
-//   tabAutoHide: false,
-//   tabAutoHideIconURL: "https://google.com/favicon.ico",
-//   tabAutoHideTitle: "Google"
-// };
+// settings
+let settings = JSON.parse(window.localStorage.getItem("settings")) || {
+  tabAutoHide: false,
+  tabAutoHideProfile: "google",
+  searchColors: false
+};
 
-// // Tab Visiblilty
-// document.addEventListener("visibilitychange", function () {
-//   if(settings.tabAutoHide) {
-//     let icon = document.getElementById("icon");
-//     if (document.hidden) {
-//       icon.href = settings.tabAutoHideIconURL;
-//       document.title = settings.tabAutoHideTitle;
-//     } else {
-//       icon.href = "img/logo-512.png";
-//       document.title = "Radon Games";
-//     }
-//   }
-// });
+// tab visibility profiles
+const visibilityProfiles = {
+  google: {
+    url: "https://www.google.com/favicon.ico",
+    title: "Google"
+  },
+  googleDrive: {
+    url: "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png",
+    title: "Google Drive"
+  },
+  gmail: {
+    url: "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico",
+    title: "Gmail"
+  },
+  khanAcademy: {
+    url: "https://www.khanacademy.org/favicon.ico",
+    title: "Khan Academy"
+  },
+  googleClassroom: {
+    url: "https://ssl.gstatic.com/classroom/favicon.png",
+    title: "Google Classroom"
+  },
+  edpuzzle: {
+    url: "https://ssl.gstatic.com/classroom/favicon.png",
+    title: "Edpuzzle"
+  },
+  quizlet: {
+    url: "https://assets.quizlet.com/a/j/dist/app/i/logo/2021/q-twilight.389092a385cb51d.png",
+    title: "Quizlet"
+  }
+}
 
-// window.localStorage.setItem("settings", JSON.stringify(settings));
+// Tab Visiblilty
+document.addEventListener("visibilitychange", function () {
+  if(settings.tabAutoHide) {
+    let icon = document.getElementById("icon");
+    if (document.hidden) {
+      icon.href = visibilityProfiles[settings.tabAutoHideProfile].url;
+      document.title = visibilityProfiles[settings.tabAutoHideProfile].title;
+    } else {
+      icon.href = "img/logo-512.png";
+      document.title = "Radon Games";
+    }
+  }
+});
+
+window.localStorage.setItem("settings", JSON.stringify(settings));
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 // initialize variables
-const express = require("express")();
-const path = require('path');
+const express = require("express");
+const app = express();
 const config = require("./config.json");
 
 // setup options
-express.use(require("express").static('public'));
-express.set('view engine', 'ejs');
+app.use(require("express").static('public'));
+app.set('view engine', 'ejs');
 // The try block is here to allow node versons below 16x
 try {
-  express.use(require("express-rate-limit")({
+  app.use(require("express-rate-limit")({
 	  windowMs: 60 * 1000,
 	  max: 60
   }));
 } catch {}
-express.use(require("express").json());
-express.use(require("express").urlencoded({ extended: true }));
+app.use(require("express").json());
+app.use(require("express").urlencoded({ extended: true }));
 
 // register routes
-require("./server/routes.js")(express);
+require("./server/routes.js")(app);
 
 // listen for requests
-express.listen(config.port || 3000);
+app.listen(config.port || 3000);
