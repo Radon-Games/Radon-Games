456bfa1629d5e83807a7c2acd777c1a631fe8880
ericksoncohen <72945444+ericksoncohen@users.noreply.github.com>
2022-01-28 21:25:56 -0600

    add donut boy

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ const config = require("./config.json");
 app.use(require("express").static('public'));
 app.set('view engine', 'ejs');
 // The try block is here to allow node versons below 16x
-try {
-  app.use(require("express-rate-limit")({
-	  windowMs: 60 * 1000,
-	  max: 60
-  }));
-} catch {}
 app.use(require("express").json());
 app.use(require("express").urlencoded({ extended: true }));
 
