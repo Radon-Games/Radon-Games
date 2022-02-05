5d1a57cb5eb3f33a7c2e90e1da1eff54186d8dfc
ericksoncohen <72945444+ericksoncohen@users.noreply.github.com>
2022-02-04 21:16:07 -0600

    fix for glitch

--- server/analytics.js
+++ server/analytics.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
   app.get("/gtag/*", (req, res) => {
     get("https://www.googletagmanager.com" + req.url).then((resp) => {
       Object.keys(resp.headers).forEach((header) => {
+        if(header === "transfer-encoding") return;
         res.setHeader(header, resp.headers[header]);
       });
       res.send(resp.data.split("https://www.google-analytics.com/g").join("").split("https://analytics.google.com/g").join(""));
