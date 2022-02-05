527bdd4f90d9751bd34fbd82bf14cfa2d8481a5c
ericksoncohen <72945444+ericksoncohen@users.noreply.github.com>
2022-02-04 20:46:34 -0600

    google analytics proxy -- TRIAL

---
+++ server/analytics.js
@@ -0,0 +1,26 @@
+const { get, post } = require("axios");
+
+module.exports = (app) => {
+  app.get("/gtag/*", (req, res) => {
+    get("https://www.googletagmanager.com" + req.url).then((resp) => {
+      Object.keys(resp.headers).forEach((header) => {
+        res.setHeader(header, resp.headers[header]);
+      });
+      res.send(resp.data.split("https://www.google-analytics.com/g").join("").split("https://analytics.google.com/g").join(""));
+    });
+  });
+  
+  app.post("/collect", (req, res) => {
+    let headers = {};
+    Object.keys(req.headers).forEach((header) => {
+      headers[header] = req.headers[header];
+    });
+    headers["host"] = "analytics.google.com";
+    post("https://www.google-analytics.com/g" + req.url, {}, { headers: headers }).then((resp) => {
+      Object.keys(resp.headers).forEach((header) => {
+        res.setHeader(header, resp.headers[header]);
+      });
+      res.send(resp.data);
+    });
+  });
+}
