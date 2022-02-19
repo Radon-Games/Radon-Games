6117ee7af90ef5e53d928f819ca484f6e1ca2219
Cohen Erickson <72945444+cohenerickson@users.noreply.github.com>
2022-02-19 16:56:00 +0000

    proxy google analytics

--- server/adsense.js
+++ server/adsense.js
@@ -1,8 +1,8 @@
 const { get } = require("axios");
 
 module.exports = (app) => {
-  app.get("/adsbygoogle.js", (req, res) => {
-    get("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8517735295733237").then((resp) => {
+  app.get("/adsbygoogle.html", (req, res) => {
+    get("https://googleads.g.doubleclick.net/pagead/html/r20220216/r20190131/zrt_lookup.html").then((resp) => {
       Object.keys(resp.headers).forEach((header) => {
         if(header === "transfer-encoding") return;
         res.setHeader(header, resp.headers[header]);
