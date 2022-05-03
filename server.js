1984b27276af2cd4ba35a33b5f973ef38f0f12d9
Cohen Erickson <cohenerickson@gmail.com>
2022-05-03 01:21:17 +0000

    g

--- server.js
+++ server.js
@@ -44,32 +44,29 @@ require("./server/routes.js")(app);
 
 
 // listen for requests
-let server;
+const HTTPSserver = https.createServer({}, app);
+const HTTPserver = http.createServer(app);
 fs.readdir("/etc/letsencrypt/live", { withFileTypes: true }, (err, files) => {
-  let s;
   if (err) {
     console.log(err);
-    server = http.createServer(app);
   } else {
-    s = true;
     const dirs = files
       .filter(d => d.isDirectory())
       .map(d => d.name);
 
-    server = https.createServer({}, app);
     dirs.forEach((dir) => {
       console.log(dir);
-      server.addContext(dir.split("-")[0], {
+      HTTPSserver.addContext(dir.split("-")[0], {
         key: fs.readFileSync(`/etc/letsencrypt/live/${dir}/privkey.pem`),
         cert: fs.readFileSync(`/etc/letsencrypt/live/${dir}/fullchain.pem`)
       });
     });
   }
 
-  if (s) server.listen(443, () => {
+  HTTPSserver.listen(443, () => {
     console.log("PORT:", 443);
   });
-  else server.listen(process.env.PORT || config.port || 3000, () => {
-    console.log("PORT:", process.env.PORT || config.port || 3000);
+  HTTPserver.listen(80, () => {
+    console.log("PORT:", 80);
   });
 });
