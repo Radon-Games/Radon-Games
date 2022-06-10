2678b814b1f4d7cc11dcc6afca34dc10b6bbb76e
Cohen Erickson <cohenerickson@gmail.com>
2022-06-10 11:57:26 -0500

    Update Bare Server

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://incog.dev/bare/',
+    bare: 'https://nebulaproxy.nebula.bio/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
