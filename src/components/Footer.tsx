257bd3d7b1ab0e83c280bbce626a370183550ff5
Cohen Erickson <cohenerickson@gmail.com>
2022-06-28 00:28:54 -0500

    Move Services

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,25 +13,26 @@ export default function Footer () {
       <p class="py-2">An open-source unblocked games website built with simplicity in mind.</p>
       <div class="py-5 px-5 sm:px-30 md:px-64 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <div>
-          <Link class="hover:underline" href="/">Home</Link><br />
-          <Link class="hover:underline" href="/games">Games</Link><br />
-          <Link class="hover:underline" href="/apps">Apps</Link><br />
+          <Link class="hover:underline" href="/">Home</Link><br/>
+          <Link class="hover:underline" href="/games">Games</Link><br/>
+          <Link class="hover:underline" href="/apps">Apps</Link><br/>
           <Link class="hover:underline" href="/services">Services</Link>
         </div>
         <div>
-          <Link class="hover:underline" href="/partners">Partners</Link><br />
-          <Link class="hover:underline" href="/supporters">Supporters</Link><br />
+          <Link class="hover:underline" href="/partners">Partners</Link><br/>
+          <Link class="hover:underline" href="/supporters">Supporters</Link><br/>
           <Link class="hover:underline" href="/settings">Settings</Link>
         </div>
         <div>
-          <Link class="hover:underline" href="/request">Game Request</Link><br />
-          <Link class="hover:underline" href="/report">Game Report</Link><br />
-          <Link class="hover:underline" href="/changelog">Changelog</Link>
+          <Link class="hover:underline" href="/request">Game Request</Link><br/>
+          <Link class="hover:underline" href="/report">Game Report</Link><br/>
+          <Link class="hover:underline" href="/changelog">Changelog</Link><br/>
+          <Link class="hover:underline" href="/privacy">Privacy</Link>
         </div>
         <div>
-          <a target="_blank" class="hover:underline" href="https://github.com/Radon-Games/Radon-Games">GitHub</a><br />
-          <a target="_blank" class="hover:underline" href="https://discord.gg/C2fbK35Rhg">Discord</a><br />
-          <a target="_blank" class="hover:underline" href="https://www.patreon.com/Radon_Games">Patreon</a><br />
+          <a target="_blank" class="hover:underline" href="https://github.com/Radon-Games/Radon-Games">GitHub</a><br/>
+          <a target="_blank" class="hover:underline" href="https://discord.gg/C2fbK35Rhg">Discord</a><br/>
+          <a target="_blank" class="hover:underline" href="https://www.patreon.com/Radon_Games">Patreon</a><br/>
           <a target="_blank" class="hover:underline" href="mailto:contact@radon.games">Contact</a>
         </div>
       </div>
--- src/routes/services.tsx
+++ src/routes/services.tsx
@@ -1,6 +1,6 @@
 import { Link } from "solid-app-router";
 
-import "../../build.css";
+import "../build.css";
 
 export default function Services () {
   document.title = "Services - Radon Games";
@@ -9,11 +9,9 @@ export default function Services () {
     <>
       <h1 class="text-2xl text-center py-10">Services</h1>
       <div class="text-center">
-        <Link href="dns" class="hover:underline">Radon DNS (Non-Blocking DNS Resolver)</Link>
+        <Link target="_blank" href="https://github.com/Radon-Games/DNS" class="hover:underline">Radon DNS</Link><br/>
+        <Link target="_blank" href="https://github.com/Radon-Games/Link-Bot" class="hover:underline">Link Bot</Link>
       </div>
-      {/* <div class="text-center">
-        <Link href="linkbot" class="hover:underline">Link Bot (Discord Link Bot)</Link>
-      </div> */}
     </>
   );
 }
