206eb7070b05ab3090912a9c080c1ec3a8e17cef
Cohen Erickson <cohenerickson@gmail.com>
2023-01-16 12:38:49 -0600

    fix missing images

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ ssr: false })],
+  build: {
+    assetsDir: ""
+  },
   server: {
     proxy: {
       "^/cdn": {
-        target: 'http://45.89.198.7'
+        target: "http://45.89.198.7"
       },
       "^/bare": {
-        target: 'http://45.89.198.7'
+        target: "http://45.89.198.7"
       }
     }
   }
