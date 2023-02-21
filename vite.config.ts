13e66917817de50ce90c2b8e7e3390d3884f6044
Cohen Erickson <cohenerickson@gmail.com>
2023-02-21 17:23:31 +0000

    Update UV config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: `${uvPath}/.`.replace(/\\/g, "/"),
+          dest: "uv"
+        },
+        {
+          src: "public/uv/uv.config.js",
+          dest: "uv"
+        }
+      ]
+    }),
+    solid({ ssr: false })
+  ],
   server: {
     proxy: {
       "^/cdn": {
