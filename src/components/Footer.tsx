28cac0edc2a7b25d885847fecef1ea622b84e59b
Cohen Erickson <cohenerickson@gmail.com>
2023-09-25 20:38:04 +0000

    Update footer and base login UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { version } from "../../package.json";
-import { Transparent } from "../assets/Transparent";
-import { Image } from "./Image";
+import { Banner } from "../assets/Banner";
 import {
+  PiGitBranchBold,
   PiGithubLogoBold,
   PiDiscordLogoBold,
   PiPatreonLogoBold
@@ -9,56 +9,50 @@ import {
 
 export function Footer() {
   return (
-    <footer class="flex w-full flex-col items-center gap-5 border-t-2 border-text-secondary px-8 py-16 md:px-16 lg:px-32  xl:px-48">
-      <div class="flex gap-5">
-        <a href="/" class="hover:text-accent-primary transition-colors">
-          Home
-        </a>
-        <a href="/games" class="hover:text-accent-primary transition-colors">
-          Games
-        </a>
-        <a href="/profile" class="hover:text-accent-primary transition-colors">
-          Profile
-        </a>
-        <a
-          href="/preferences"
-          class="hover:text-accent-primary transition-colors"
-        >
-          Preferences
-        </a>
-        <a href="/privacy" class="hover:text-accent-primary transition-colors">
-          Privacy
-        </a>
+    <footer class="flex justify-between gap-8 border-t-2 border-text-secondary px-8 py-16 md:px-16 lg:px-32 xl:px-48">
+      <div class="flex flex-1 flex-col items-start gap-5">
+        <Banner class="h-6" />
+        <p class="text-xs">
+          An open-source unblocked games website built with simplicity in mind.
+        </p>
+        <div class="flex items-center gap-5">
+          <a
+            href="https://github.com/Radon-Games/Radon-Games"
+            class="transition-colors hover:text-accent-primary"
+          >
+            <PiGithubLogoBold />
+          </a>
+          <a
+            href="https://discord.gg/unblock"
+            class="transition-colors hover:text-accent-primary"
+          >
+            <PiDiscordLogoBold />
+          </a>
+          <a
+            href="https://www.patreon.com/Radon_Games"
+            class="transition-colors hover:text-accent-primary"
+          >
+            <PiPatreonLogoBold />
+          </a>
+          <a
+            href={`https://github.com/Radon-Games/Radon-Games/releases/tag/v${version}`}
+            target="_blank"
+            class="flex items-center gap-2 hover:text-accent-primary"
+          >
+            <PiGitBranchBold /> v{version}
+          </a>
+        </div>
       </div>
-      <div class="flex gap-5 text-xl">
-        <a
-          href="https://github.com/Radon-Games/Radon-Games"
-          class="hover:text-accent-primary transition-colors"
-        >
-          <PiGithubLogoBold />
-        </a>
-        <a
-          href="https://discord.gg/unblock"
-          class="hover:text-accent-primary transition-colors"
-        >
-          <PiDiscordLogoBold />
-        </a>
-        <a
-          href="https://www.patreon.com/Radon_Games"
-          class="hover:text-accent-primary transition-colors"
-        >
-          <PiPatreonLogoBold />
-        </a>
-      </div>
-      <div class="flex gap-5">
-        <Transparent class="h-5" />
-        <a
-          class="hover:text-accent-primary transition-colors"
-          href={`https://github.com/Radon-Games/Radon-Games/releases/tag/v${version}`}
-          target="_blank"
-        >
-          v{version}
-        </a>
+      <div class="flex flex-1 flex-wrap items-center justify-end gap-5">
+        <a class="hover:text-accent-primary">Home</a>
+        <a class="hover:text-accent-primary">Games</a>
+        <a class="hover:text-accent-primary">Search</a>
+        <a class="hover:text-accent-primary">Login</a>
+        <a class="hover:text-accent-primary">Register</a>
+        <a class="hover:text-accent-primary">Profile</a>
+        <a class="hover:text-accent-primary">Preferences</a>
+        <a class="hover:text-accent-primary">Shop</a>
+        <a class="hover:text-accent-primary">Privacy</a>
       </div>
     </footer>
   );
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,12 +1,86 @@
+import { Transparent } from "../assets/Transparent";
 import { motion } from "framer-motion";
+import { useState } from "preact/hooks";
 
 export function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleLogin() {
+    setIsLoading(true);
+  }
+
   return (
     <motion.main
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -10 }}
-      class="px-8 md:px-16 lg:px-32 xl:px-48"
-    ></motion.main>
+      class="flex flex-col items-center justify-center gap-5 px-8 py-16 md:px-16 lg:px-32 xl:px-48"
+    >
+      <Transparent class="h-8" />
+      <h1 class="text-center text-2xl">Sign in to your account</h1>
+      <div class="flex flex-col gap-2">
+        <label for="username">Username</label>
+        <input
+          id="username"
+          class="rounded-md border border-bg-secondary bg-transparent px-2 py-1 text-base font-normal outline-accent-secondary ring-accent-primary focus:outline-0 focus:ring-2"
+          type="text"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label for="username">Password</label>
+        <input
+          id="username"
+          class="rounded-md border border-bg-secondary bg-transparent px-2 py-1 text-base font-normal outline-accent-secondary ring-accent-primary focus:outline-0 focus:ring-2"
+          type="password"
+        />
+      </div>
+      <motion.div
+        class={`flex ${
+          isLoading ? "cursor-not-allowed" : "cursor-pointer"
+        } curos items-center justify-center gap-2 rounded-lg bg-accent-secondary px-4 py-2 font-semibold shadow-lg`}
+        whileHover={{
+          scale: 1.05,
+          boxShadow: "0px 0px 16px rgb(55 48 163)",
+          transition: {
+            duration: 0.2
+          }
+        }}
+        onClick={handleLogin}
+      >
+        {!isLoading ? (
+          "Login"
+        ) : (
+          <span class="flex items-center justify-center gap-3">
+            <svg
+              class="h-5 w-5 animate-spin text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span>Loading...</span>
+          </span>
+        )}
+      </motion.div>
+
+      <div class="flex justify-center">
+        <hr class="flex-1 h-[2px] bg-text-secondary"/>
+        <span class="px-2">OR</span>
+        <hr class="flex-1 h-[2px] bg-text-secondary"/>
+      </div>
+    </motion.main>
   );
 }
