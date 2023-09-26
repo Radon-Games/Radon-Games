24a60cf7ad31abf5f09446c83e36e75a9e08072a
Cohen Erickson <cohenerickson@gmail.com>
2023-09-25 22:26:46 -0500

    remove csp

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [million.vite({ mode: "preact" })],
   server: {
-    headers: {
-      "Content-Security-Policy":
-        "default-src 'self' *.google-analytics.com pagead2.googlesyndication.com;style-src 'self' 'unsafe-inline';script-src 'self' 'unsafe-inline' *.googletagmanager.com pagead2.googlesyndication.com;"
-    },
     proxy: {
       "/cdn": {
         target: "https://cdn.radon.games/",
